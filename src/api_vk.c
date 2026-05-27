4ad3668fea495d1a9bf1c798075ee93a2f9641c0
Danylo Piliaiev <dpiliaiev@igalia.com>
2026-05-27 10:59:51 +0200

    vk: Don't create multi-sample storage images
    The tests don't do image writes on multi-sampled images, so don't
    ask for them, since they are may not be supported. E.g. they are not
    on Adreno GPUs.
    
    Signed-off-by: Danylo Piliaiev <dpiliaiev@igalia.com>

--- src/api_vk.c
+++ src/api_vk.c
@@ -320,9 +320,9 @@ vk_create_image(api_context *ctx, VkImageType type, VkFormat format, unsigned wi
                              .usage = is_zs ?
                                          VK_IMAGE_USAGE_DEPTH_STENCIL_ATTACHMENT_BIT :
                                          VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT |
-                                         VK_IMAGE_USAGE_STORAGE_BIT |
                                          VK_IMAGE_USAGE_TRANSFER_DST_BIT |
-                                         VK_IMAGE_USAGE_TRANSFER_SRC_BIT,
+                                         VK_IMAGE_USAGE_TRANSFER_SRC_BIT |
+                                         ((samples == 1) ? VK_IMAGE_USAGE_STORAGE_BIT : 0) ,
                           },
                           NULL, &image->image));
 
